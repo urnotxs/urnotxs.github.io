fa5d026ead59a09a596b66939e36c45225d9dc28
urnot-XS <532771504@qq.com>
2019-01-18 17:57:48 +0800

    blog

--- dist/music.js
+++ dist/music.js
@@ -9,18 +9,21 @@ const ap = new APlayer({
         url: 'http://www.ytmp3.cn/down/55639.mp3',
         cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
       },{
+        name: '流行',
+        artist: '李宇春',
+        url: 'http://www.170mv.com/kw/other.web.nf01.sycdn.kuwo.cn/resource/n2/56/8/1886937956.mp3',
+        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+      },{
         name: '一趟',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/49285.mp3',
         cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
-      },
-      {
+      },{
         name: '下个路口见',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/50718.mp3',
         cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
-      },
-      {
+      },{
         name: '存在感',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/48581.mp3',
