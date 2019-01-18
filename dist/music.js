fff4706b769911d8aa61d1502131aebbcff8b4ab
urnot-XS <532771504@qq.com>
2019-01-18 17:43:39 +0800

    blog

--- dist/music.js
+++ dist/music.js
@@ -7,24 +7,24 @@ const ap = new APlayer({
         name: '木兰',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/55639.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
       },{
         name: '一趟',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/49285.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
       },
       {
         name: '下个路口见',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/50718.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
       },
       {
         name: '存在感',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/48581.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
       }
     ]
 });
