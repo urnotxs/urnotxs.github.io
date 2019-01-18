9d54d2d3da2b1e5eb1f22dc5a1bee1b7da48d91f
urnot-XS <532771504@qq.com>
2019-01-18 18:17:34 +0800

    blog

--- dist/music.js
+++ dist/music.js
@@ -7,27 +7,27 @@ const ap = new APlayer({
         name: '木兰',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/55639.mp3',
-        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/ImageLib/master/1987.jpg',
       },{
         name: '流行',
         artist: '李宇春',
         url: 'http://www.170mv.com/kw/other.web.nf01.sycdn.kuwo.cn/resource/n2/56/8/1886937956.mp3',
-        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/ImageLib/master/1987.jpg',
       },{
         name: '一趟',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/49285.mp3',
-        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/ImageLib/master/1987.jpg',
       },{
         name: '下个路口见',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/50718.mp3',
-        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/ImageLib/master/cover.jpg',
       },{
         name: '存在感',
         artist: '李宇春',
         url: 'http://www.ytmp3.cn/down/48581.mp3',
-        cover: 'https://raw.githubusercontent.com/urnotxs/urnotxs.github.io/master/music_cover.jpg',
+        cover: 'https://raw.githubusercontent.com/urnotxs/ImageLib/master/cover.jpg',
       }
     ]
 });
